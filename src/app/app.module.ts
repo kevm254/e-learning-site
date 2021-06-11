45571866b318112ba845501d938d889b1301304f
Kev Mil <kevm2@yahoo.com>
2021-06-10 22:22:16 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NavbarComponent} from '../components/navbar/navbar.component';
+import {HeroPanelComponent} from '../components/panels/hero-panel/hero-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroPanelComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/components/panels/hero-panel/hero-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hero-panel-cmp',
+  templateUrl: './hero-panel.component.html',
+  styleUrls: ['./hero-panel.component.scss']
+})
+export class HeroPanelComponent {
+
+}
